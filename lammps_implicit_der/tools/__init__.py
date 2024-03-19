3fdf009995cc9d0223e5534d71a2f731b284731a
imaliyov <ivan.maliyov@gmail.com>
2024-03-19 12:41:41 +0100

    get_size function

--- lammps_implicit_der/tools/__init__.py
+++ lammps_implicit_der/tools/__init__.py
@@ -3,4 +3,4 @@ Tools: timing, memory, errors, etc.
 """
 from .timing import Timing, TimingGroup
 from .utils import mpi_print, get_default_data_path, \
-                   initialize_mpi
+                   initialize_mpi, get_size
